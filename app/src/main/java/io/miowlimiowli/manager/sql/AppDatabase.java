4c515d21377c7664f5522b5ae92324216c068672
sunjunbo <sunjunbo@gmail.com>
2019-08-29 22:32:15 +0800

    评论SQL（未完成）

--- app/src/main/java/io/miowlimiowli/manager/sql/AppDatabase.java
+++ app/src/main/java/io/miowlimiowli/manager/sql/AppDatabase.java
@@ -2,8 +2,11 @@ package io.miowlimiowli.manager.sql;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+
+import io.miowlimiowli.manager.Comment;
+
 //TODO:exportSchema = false for test
-@Database(entities = {SqlNews.class, SqlUserandNews.class}, version = 1, exportSchema = false)
+@Database(entities = {SqlNews.class, SqlUserandNews.class, Comment.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract SqlNewsDao SqlNewsDao();
     public abstract  SqlUserandNewsDao SqlUserandNewsDao();
