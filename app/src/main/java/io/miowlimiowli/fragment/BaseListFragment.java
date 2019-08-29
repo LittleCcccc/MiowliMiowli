aaf2a6535de0d98d1b4f5e07a489ccc62b30bd1a
TongYuchang <tyc16@qq.com>
2019-08-30 05:08:16 +0800

    标签增删，收藏评论历史页面,下拉刷新，底部上拉加载更多。

--- app/src/main/java/io/miowlimiowli/fragment/BaseListFragment.java
+++ app/src/main/java/io/miowlimiowli/fragment/BaseListFragment.java
@@ -1,6 +1,7 @@
 package io.miowlimiowli.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,34 +35,65 @@ abstract public class BaseListFragment extends Fragment {
     protected NewsListAdapter newsListAdapter;
     protected RecyclerView.LayoutManager mLayoutManager;
 
-    protected int mPageSize = 100;
+    protected int mPageSize = 3;
     protected int mPageNo = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.news_list, container, false);
-        init();
-        /*
+
+
         mSwipeRefreshWidget = (SuperSwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_widget);
         mSwipeRefreshWidget.setOnPullRefreshListener(new SuperSwipeRefreshLayout.OnPullRefreshListener(){
             @Override
             public void onRefresh(){
                 refreshNews();
+                mSwipeRefreshWidget.setRefreshing(false);
             }
             @Override
             public void onPullDistance(int distance){
-                if(distance>2)
-                {
-                    requireMoreNews();
-                }
             }
             @Override
             public void onPullEnable(boolean enable){
 
             }
         });
-        */
+        mSwipeRefreshWidget.setOnPushLoadMoreListener(new SuperSwipeRefreshLayout.OnPushLoadMoreListener(){
+            @Override
+            public void onLoadMore(){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        requireMoreNews();
+                        mSwipeRefreshWidget.setLoadMore(false);
+                    }
+                },5000);
+            }
+            @Override
+            public void onPushEnable(boolean enable) {
+                // TODO 上拉过程中，上拉的距离是否足够出发刷新
+            }
 
+            @Override
+            public void onPushDistance(int distance) {
+                // TODO 上拉距离
+
+            }
+        });
+
+        // Configure Recommend component
+        mRecyclerView = view.findViewById(R.id.news_recycler_view);
+
+        mLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        newsListAdapter = new NewsListAdapter(getContext());
+        mRecyclerView.setAdapter(newsListAdapter);
+
+
+        DividerItemDecoration recommendRecyclerViewDecoration = new DividerItemDecoration(this.getContext(), LinearLayoutManager.VERTICAL);
+        recommendRecyclerViewDecoration.setDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.recommend_activity_recommend_recycler_view_separator));
+        mRecyclerView.addItemDecoration(recommendRecyclerViewDecoration);
 
         return view;
     }
@@ -79,23 +111,6 @@ abstract public class BaseListFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    public void init(){
-
-        // Configure Recommend component
-        mRecyclerView = this.getView().findViewById(R.id.news_recycler_view);
-
-        mLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        newsListAdapter = new NewsListAdapter(getContext());
-        mRecyclerView.setAdapter(newsListAdapter);
-
-
-        DividerItemDecoration recommendRecyclerViewDecoration = new DividerItemDecoration(this.getContext(), LinearLayoutManager.VERTICAL);
-        recommendRecyclerViewDecoration.setDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.recommend_activity_recommend_recycler_view_separator));
-        mRecyclerView.addItemDecoration(recommendRecyclerViewDecoration);
-
-    }
 
     public void setNewsList(List<DisplayableNews> list)
     {
